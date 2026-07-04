da2727e868b2f52388f40fe3ede03520c09439a7
Ahmed Raza <ahmedrazalohar24@gmail.com>
2026-07-04 19:50:21 +0530

    get method

--- index.js
+++ index.js
@@ -2,4 +2,13 @@ const express = require("express")
 
 const app = express()
 
+app.get('/', (req, res)=>{
+    res.send("Hello world")
+})
+
+
+app.get("/about", (req ,res)=>{
+    res.send("This is about page")
+})
+
 app.listen(3000)
