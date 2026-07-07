3b7a78cf21a3de63643b606c96a0241a663b9741
Ahmed Raza <ahmedrazalohar24@gmail.com>
2026-07-07 20:00:12 +0530

    post api

--- server.js
+++ server.js
@@ -6,13 +6,7 @@ app.listen(3000, ()=>{
     console.log("server is running on port 3000")
 })
 
-const notes = [
-    {
-        title:"test title 1",
-        description:"test description 1"
-    },
-    {
-        title:"test title 2",
-        description:"test description 2"
-    },
-]
+
+app.post("/notes", (req, res)=>{
+    res.send("note created")
+})
