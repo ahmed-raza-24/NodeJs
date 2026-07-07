23a29f3241f21bb9874de25636814746f7ba4368
Ahmed Raza <ahmedrazalohar24@gmail.com>
2026-07-07 20:26:44 +0530

    post api

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require("express")
 
 const app = express()
 
+app.use(express.json())
+
 app.listen(3000, ()=>{
     console.log("server is running on port 3000")
 })
 
 
 app.post("/notes", (req, res)=>{
+    console.log(req.body)
     res.send("note created")
 })
