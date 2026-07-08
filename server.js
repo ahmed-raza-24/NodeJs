574b3ffee73a9aa9d7b96b962c19f4c503c62f7c
Ahmed Raza <ahmedrazalohar24@gmail.com>
2026-07-08 20:18:23 +0530

    get api

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express()
 
 app.use(express.json())
 
+const notes = []
+
 app.listen(3000, ()=>{
     console.log("server is running on port 3000")
 })
@@ -11,5 +13,12 @@ app.listen(3000, ()=>{
 
 app.post("/notes", (req, res)=>{
     console.log(req.body)
+
+    notes.push(req.body)
+
     res.send("note created")
+})
+
+app.get("/notes", (req, res)=>{
+    res.send(notes)
 })
