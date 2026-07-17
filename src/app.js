f591cb768a505a84bc3f4e155d2e062935ac8857
Ahmed Raza <ahmedrazalohar24@gmail.com>
2026-07-17 21:08:48 +0530

    get api with asyn & await

--- src/app.js
+++ src/app.js
@@ -20,5 +20,14 @@ app.post("/notes", async (req, res)=>{
     })
 })
 
+
+
+app.get("/notes", async (req, res)=>{
+    const notes = await noteModel.find()
+
+    res.status(200).json({
+        message: "Notes fetected successfully"
+    })
+})
 module.exports = app
 
